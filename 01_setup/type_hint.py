8c06c0453a7e107af79995bb0f17856b5a579469
Kagami <shimura.kyohei7617@gmail.com>
2024-05-21 22:41:41 +0900

    auto: auto command push

--- 01_setup/type_hint.py
+++ 01_setup/type_hint.py
@@ -11,3 +11,9 @@ f: dict[str, bool] = {"foo": True, "bar": False}
 
 # Literal: Enumのように使用可能 + 引数値のチェックに使用可能
 g: Literal["OK", "NG"] = "OK"
+
+
+def sample(x: str) -> bool:
+    if x == "OK":
+        return True
+    return False
