891510bd26c408ec75a1f7d419dfa4379b7ec4f1
Kagami <shimura.kyohei7617@gmail.com>
2024-05-22 22:08:07 +0900

    auto: auto command push

--- 02_uml/class_diagram_practice/practice1.py
+++ 02_uml/class_diagram_practice/practice1.py
@@ -1,9 +1,9 @@
 # クラス図の実装
 class Employee:
-    def __init__(self, id, name, salary):
-        self.emp_id: int = id
-        self.name: str = name
-        self.salary: int = salary
+    def __init__(self, emp_id: int, name: str, salary: int):
+        self.__emp_id = emp_id
+        self.__name = name
+        self.__salary = salary
 
     def _work(self):
         print(f"{self.name} is working")
