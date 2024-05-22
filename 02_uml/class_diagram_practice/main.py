02c46855d2199fde3e3a378aa71458ea0956bd07
Kagami <shimura.kyohei7617@gmail.com>
2024-05-22 22:20:52 +0900

    auto: auto command push

--- 02_uml/class_diagram_practice/main.py
+++ 02_uml/class_diagram_practice/main.py
@@ -3,6 +3,6 @@ from practice1 import Employee
 if __name__ == "__main__":
     employee = Employee(1, "John", 10000)
     employee._work()
-    print(employee.salary())
-    employee.salary(20000)
-    print(employee.salary())
+    print(employee.salary)
+    employee.salary = 20000
+    print(employee.salary)
