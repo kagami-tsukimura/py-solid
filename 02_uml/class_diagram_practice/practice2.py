07dba968381e9b43593ad17b9ced41edc830050b
Kagami <shimura.kyohei7617@gmail.com>
2024-05-23 20:37:09 +0900

    auto: auto command push

--- 02_uml/class_diagram_practice/practice2.py
+++ 02_uml/class_diagram_practice/practice2.py
@@ -6,3 +6,13 @@ class Shape(metaclass=ABCMeta):
     @abstractmethod
     def calc_area(self) -> int:
         pass
+
+
+class Rectangle(Shape):
+
+    def __init__(self, width: int, height: int):
+        self.__width = width
+        self.__height = height
+
+    def calc_area(self) -> int:
+        return self.__width * self.__height
