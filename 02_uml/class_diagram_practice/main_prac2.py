4f61296067d23886a5926db9998c75408dbe5270
Kagami <shimura.kyohei7617@gmail.com>
2024-05-23 20:41:05 +0900

    auto: auto command push

--- 02_uml/class_diagram_practice/main_prac2.py
+++ 02_uml/class_diagram_practice/main_prac2.py
@@ -2,13 +2,12 @@ from practice2 import Rectangle, Square
 
 
 def main() -> None:
-    pass
-
-
-if __name__ == "__main__":
-    main()
     rect = Rectangle(10, 20)
     print(rect.calc_area())
 
     square = Square(10)
     print(square.calc_area())
+
+
+if __name__ == "__main__":
+    main()
