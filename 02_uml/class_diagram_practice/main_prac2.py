8b80c1a733fca3ff36fdf49daa4a21b19d2b64de
Kagami <shimura.kyohei7617@gmail.com>
2024-05-23 20:40:38 +0900

    auto: auto command push

--- 02_uml/class_diagram_practice/main_prac2.py
+++ 02_uml/class_diagram_practice/main_prac2.py
@@ -1,8 +1,8 @@
-from practice2 import Employee
+from practice2 import Rectangle, Square
 
 if __name__ == "__main__":
-    employee = Employee(1, "John", 10000)
-    employee._work()
-    print(employee.salary)
-    employee.salary = 20000
-    print(employee.salary)
+    rect = Rectangle(10, 20)
+    print(rect.calc_area())
+
+    square = Square(10)
+    print(square.calc_area())
