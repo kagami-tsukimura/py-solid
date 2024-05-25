853830326bc34f3b5b32b1a8fab7b7a5b1ac5ebe
Kagami <shimura.kyohei7617@gmail.com>
2024-05-25 12:32:41 +0900

    auto: auto command push

--- 03_solid_principle/open_closed/good_example.py
+++ 03_solid_principle/open_closed/good_example.py
@@ -9,3 +9,41 @@ class IEmployee(metaclass=ABCMeta):
     @abstractmethod
     def get_bonus(self, base: int) -> int:
         pass
+
+
+class Junior(IEmployee):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def get_bonus(self, base: int) -> int:
+        return math.floor(base * 1.1)
+
+
+class Middle(IEmployee):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def get_bonus(self, base: int) -> int:
+        return math.floor(base * 1.5)
+
+
+class Senior(IEmployee):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def get_bonus(self, base: int) -> int:
+        return math.floor(base * 2)
+
+
+class Expert(IEmployee):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+
+    def get_bonus(self, base: int) -> int:
+        return math.floor(base * 3)
+
+
+if __name__ == "__main__":
+    junior = Junior("Yamada")
+
+    print(junior.get_bonus(100))
