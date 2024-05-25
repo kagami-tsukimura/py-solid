df607bdb3a0972e4aa67dc930bc58cd57caa1c75
Kagami <shimura.kyohei7617@gmail.com>
2024-05-25 20:56:11 +0900

    auto: auto command push

---
+++ 03_solid_principle/dependency_inversion/good_example.py
@@ -0,0 +1,52 @@
+from abc import ABCMeta, abstractmethod
+
+
+class User:
+    pass
+
+
+class IUserService(metaclass=ABCMeta):
+    @abstractmethod
+    def create(self, user: User) -> User:
+        pass
+
+    @abstractmethod
+    def find_by_id(self, id: str) -> User:
+        pass
+
+
+class UserController:
+    def __init__(self, user_service: IUserService):
+        self.__user_service = user_service
+
+    def create(self, user: User) -> User:
+        return self.__user_service.create(user)
+
+    def find_by_id(self, id: str) -> User:
+        return self.__user_service.find_by_id(id)
+
+
+class UserService:
+    def __init__(self):
+        self.__user_repository = UserRdbRepository()
+
+    def create(self, user: User) -> User:
+        return self.__user_repository.create(user)
+
+    def find_by_id(self, id: str) -> User:
+        return self.__user_repository.find_by_id(id)
+
+
+class UserRdbRepository:
+    def create(self, user: User) -> User:
+        print("RDBにUserを登録")
+        return user
+
+    def find_by_id(self, id: str) -> User:
+        print(f"ID: {id}のユーザーを検索")
+        return User()
+
+
+if __name__ == "__main__":
+    user_controller = UserController()
+    user_controller.find_by_id("123")
