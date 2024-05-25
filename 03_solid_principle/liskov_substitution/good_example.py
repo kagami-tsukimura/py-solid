131ad7ba8c0dcd8c3310e2735303fc370404a15c
Kagami <shimura.kyohei7617@gmail.com>
2024-05-25 15:56:19 +0900

    auto: auto command push

--- 03_solid_principle/liskov_substitution/good_example.py
+++ 03_solid_principle/liskov_substitution/good_example.py
@@ -29,12 +29,31 @@ class Rectangle(Shape):
         self.__height = height
 
     def get_area(self) -> int:
-        return self.__width * self.__height
+        return self.width * self.height
+
+
+class Square(Shape):
+    def __init__(self):
+        self.__length = 0
+
+    @property
+    def length(self) -> int:
+        return self.__length
+
+    @length.setter
+    def length(self, length: int):
+        self.__length = length
+
+    def get_area(self) -> int:
+        return self.length**2
 
 
 if __name__ == "__main__":
-    r = Rectangle(width=10, height=20)
-    print(r.get_area())
+    rectangle = Rectangle()
+    rectangle.width = 10
+    rectangle.height = 30
+    print(rectangle.get_area())
 
-    square = Rectangle(width=10, height=10)
+    square = Square()
+    square.length = 15
     print(square.get_area())
