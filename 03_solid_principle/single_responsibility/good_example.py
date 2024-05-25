c04d57fbf37702d4acb7724bec5448d9a11578ca
Kagami <shimura.kyohei7617@gmail.com>
2024-05-25 11:47:06 +0900

    auto: auto command push

--- 03_solid_principle/single_responsibility/good_example.py
+++ 03_solid_principle/single_responsibility/good_example.py
@@ -8,31 +8,33 @@ class PayCalculator:
     def __get_regular_hours(self):
         print("経理部門の仕様変更済み")
 
-    def calculate_pay(self):
+    def calculate_pay(self, employee: EmployeeData):
         self.__get_regular_hours()
-        print(f"{self.name}の給与を計算しました")
+        print(f"{employee.name}の給与を計算しました")
 
 
 class HourReporter:
     def __get_regular_hours(self):
         print("人事部門の仕様変更済み")
 
-    def report_hours(self):
+    def report_hours(self, employee: EmployeeData):
         self.__get_regular_hours()
-        print(f"{self.name}の労働時間をレポートしました")
+        print(f"{employee.name}の労働時間をレポートしました")
 
 
 class EmployeeRepository:
-    def save(self):
-        print(f"{self.name}を保存しました")
+    def save(self, employee: EmployeeData):
+        print(f"{employee.name}を保存しました")
 
 
 if __name__ == "__main__":
     emp = EmployeeData("山田", "開発")
+    calc = PayCalculator()
+    rep = HourReporter()
 
     print("経理部門")
-    emp.calculate_pay()
+    calc.calculate_pay()
 
     print("")
     print("人事部門")
-    emp.report_hours()
+    rep.report_hours()
