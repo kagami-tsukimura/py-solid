d3788a78032abfcf108dfe73c5273a25eeb12548
Kagami <shimura.kyohei7617@gmail.com>
2024-05-26 14:08:14 +0900

    auto: auto command push

--- 04_design_pattern_part1/iterator/iterator.py
+++ 04_design_pattern_part1/iterator/iterator.py
@@ -1,1 +1,10 @@
 from abc import ABCMeta, abstractmethod
+
+
+class Patient:
+    def __init__(self, id: int, name: str) -> None:
+        self.id = id
+        self.name = name
+
+    def __str__(self) -> str:
+        return f"{self.id}: {self.name}"
