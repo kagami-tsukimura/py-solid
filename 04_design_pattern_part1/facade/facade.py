0626188a152ff8ed56790d495cf78b7a89b23b8a
Kagami <shimura.kyohei7617@gmail.com>
2024-05-26 15:16:49 +0900

    auto: auto command push

--- 04_design_pattern_part1/facade/facade.py
+++ 04_design_pattern_part1/facade/facade.py
@@ -14,12 +14,12 @@ class Invoice:
 
 
 class Order:
-    def __init__(self, product: Product, payment: Payment, invoice: Invoice):
-        self.product = product
-        self.payment = payment
-        self.invoice = invoice
-
-    def make_order(self, name: str):
-        self.product.get_product(name)
-        self.payment.make_payment(name)
-        self.invoice.send_invoice(name)
+    def place_order(self, name: str):
+        print("注文開始")
+        product = Product()
+        product.get_product(name)
+        payment = Payment()
+        payment.make_payment(name)
+        invoice = Invoice()
+        invoice.send_invoice(name)
+        print("注文完了")
