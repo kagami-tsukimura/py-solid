9f4ef151e0b01db3fd866e81cd4be741f08bca53
Kagami <shimura.kyohei7617@gmail.com>
2024-05-26 15:15:20 +0900

    auto: auto command push

---
+++ 04_design_pattern_part1/facade/facade.py
@@ -0,0 +1,13 @@
+class Product:
+    def get_product(self, name: str):
+        print(f"{name}を取得しました。")
+
+
+class Payment:
+    def make_payment(self, name: str):
+        print(f"{name}を支払いました。")
+
+
+class Invoice:
+    def send_invoice(self, name: str):
+        print(f"{name}の請求書を作成しました。")
