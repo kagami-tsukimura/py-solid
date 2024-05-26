757d0a5e66684b34ed3f214938e8bf2674ecd2bb
Kagami <shimura.kyohei7617@gmail.com>
2024-05-26 15:32:58 +0900

    auto: auto command push

--- 05_design_pattern_part2/prototype/prototype.py
+++ 05_design_pattern_part2/prototype/prototype.py
@@ -2,3 +2,17 @@ from __future__ import annotations
 
 from abc import ABCMeta, abstractmethod
 from copy import deepcopy
+
+
+class ItemPrototype(metaclass=ABCMeta):
+    def __init__(self, name: str) -> None:
+        self.__name = name
+        self.__review: list[str] = []
+
+    def __str__(self) -> str:
+        return f"{self.__name}: {self.__review}"
+
+    @abstractmethod
+    def clone(self) -> ItemPrototype:
+        """原型を複製する"""
+        pass
