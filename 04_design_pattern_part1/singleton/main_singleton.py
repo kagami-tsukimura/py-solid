c2ec6539a430c9ede13ec65b9e4890a240963302
Kagami <shimura.kyohei7617@gmail.com>
2024-05-26 11:11:21 +0900

    auto: auto command push

--- 04_design_pattern_part1/singleton/main_singleton.py
+++ 04_design_pattern_part1/singleton/main_singleton.py
@@ -2,7 +2,11 @@ from singleton import Logger
 from test_class import Test
 
 if __name__ == "__main__":
-    log = Logger()
-    log.output("hello")
-
-    t = Test()
+    t1 = Test()
+    t2 = Test()
+    print(f"Test: {t1 == t2}")
+    l1 = Logger()
+    l2 = Logger()
+    print(f"Singleton: {l1 == l2}")
+    l1.output("hello")
+    l2.output("world")
