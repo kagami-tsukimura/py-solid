b068bb69cc7902e11558a062299447b79c50eea4
Kagami <shimura.kyohei7617@gmail.com>
2024-05-26 13:11:38 +0900

    auto: auto command push

--- 04_design_pattern_part1/adapter/adapter_inheritance.py
+++ 04_design_pattern_part1/adapter/adapter_inheritance.py
@@ -5,3 +5,8 @@ class Target(metaclass=ABCMeta):
     @abstractmethod
     def get_csv_data(self) -> str:
         pass
+
+
+class NewLibrary:
+    def get_json_data(self) -> list[dict[str, str]]:
+        pass
