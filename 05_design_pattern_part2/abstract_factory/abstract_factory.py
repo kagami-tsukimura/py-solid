3813c3b59f6c0f584d7be5db23486ea8f8522456
Kagami <shimura.kyohei7617@gmail.com>
2024-05-27 21:13:40 +0900

    auto: auto command push

--- 05_design_pattern_part2/abstract_factory/abstract_factory.py
+++ 05_design_pattern_part2/abstract_factory/abstract_factory.py
@@ -11,3 +11,13 @@ class Checkbox(metaclass=ABCMeta):
     @abstractmethod
     def switch(self) -> None:
         pass
+
+
+class GUIFactory(metaclass=ABCMeta):
+    @abstractmethod
+    def create_button(self) -> Button:
+        pass
+
+    @abstractmethod
+    def create_checkbox(self) -> Checkbox:
+        pass
