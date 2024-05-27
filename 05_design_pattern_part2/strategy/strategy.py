0afb4c84baab4f3d3577e4a49ed1f6b249a9cc8a
Kagami <shimura.kyohei7617@gmail.com>
2024-05-27 21:34:45 +0900

    auto: auto command push

--- 05_design_pattern_part2/strategy/strategy.py
+++ 05_design_pattern_part2/strategy/strategy.py
@@ -5,3 +5,8 @@ class PaymentStrategy(metaclass=ABCMeta):
     @abstractmethod
     def pay(self, amount: int) -> None:
         pass
+
+
+class CashPaymentStrategy(PaymentStrategy):
+    def pay(self, amount: int) -> None:
+        print(f"Paid in cash for {amount}.")
