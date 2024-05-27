12274403fd9398101b08a59fcfcb87ef020e7dd3
Kagami <shimura.kyohei7617@gmail.com>
2024-05-27 21:13:24 +0900

    auto: auto command push

--- 05_design_pattern_part2/abstract_factory/abstract_factory.py
+++ 05_design_pattern_part2/abstract_factory/abstract_factory.py
@@ -1,13 +1,13 @@
 from abc import ABCMeta, abstractmethod
 
 
-class Checkbox(metaclass=ABCMeta):
-    @abstractmethod
-    def switch(self) -> None:
-        pass
-
-
 class Button(metaclass=ABCMeta):
     @abstractmethod
     def press(self) -> None:
+        pass
+
+
+class Checkbox(metaclass=ABCMeta):
+    @abstractmethod
+    def switch(self) -> None:
         pass
