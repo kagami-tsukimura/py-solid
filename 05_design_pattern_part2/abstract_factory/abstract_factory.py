2e34113f6da03a0c18f06907ac5330169d952398
Kagami <shimura.kyohei7617@gmail.com>
2024-05-27 21:15:08 +0900

    auto: auto command push

--- 05_design_pattern_part2/abstract_factory/abstract_factory.py
+++ 05_design_pattern_part2/abstract_factory/abstract_factory.py
@@ -21,3 +21,29 @@ class GUIFactory(metaclass=ABCMeta):
     @abstractmethod
     def create_checkbox(self) -> Checkbox:
         pass
+
+
+class WindowsButton(Button):
+    def press(self) -> None:
+        print("WindowsButton is pressed")
+
+
+class WindowsCheckbox(Checkbox):
+    def switch(self) -> None:
+        print("WindowsCheckbox is switched")
+
+
+class WindowsGUIFactory(GUIFactory):
+    def create_button(self) -> Button:
+        return WindowsButton()
+
+    def create_checkbox(self) -> Checkbox:
+        return WindowsCheckbox()
+
+
+if __name__ == "__main__":
+    factory = WindowsGUIFactory()
+    button = factory.create_button()
+    checkbox = factory.create_checkbox()
+    button.press()
+    checkbox.switch()
