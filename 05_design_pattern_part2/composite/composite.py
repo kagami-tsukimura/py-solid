94e20111d666fc958650171b48d5dfc385abd579
Kagami <shimura.kyohei7617@gmail.com>
2024-05-28 21:18:33 +0900

    auto: auto command push

--- 05_design_pattern_part2/composite/composite.py
+++ 05_design_pattern_part2/composite/composite.py
@@ -4,3 +4,15 @@ from abc import ABCMeta, abstractmethod
 class Entry(metaclass=ABCMeta):
     def __init__(self, name: str) -> None:
         self.__name = name
+
+    @property
+    def name(self) -> str:
+        return self.__name
+
+    @abstractmethod
+    def get_size(self) -> int:
+        pass
+
+    @abstractmethod
+    def remove(self) -> None:
+        pass
