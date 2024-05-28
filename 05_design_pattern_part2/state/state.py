1aaa81f7ce210469b6c966ea852c0572833f72e6
Kagami <shimura.kyohei7617@gmail.com>
2024-05-28 19:33:41 +0900

    auto: auto command push

--- 05_design_pattern_part2/state/state.py
+++ 05_design_pattern_part2/state/state.py
@@ -7,3 +7,29 @@ class LightState(metaclass=ABCMeta):
     @abstractmethod
     def switch(self):
         pass
+
+
+class OffState(LightState):
+    def switch(self) -> LightState:
+        print("Light is on")
+        return OnState()
+
+
+class OnState(LightState):
+    def switch(self) -> LightState:
+        print("Light is off")
+        return OffState()
+
+
+class LightSwitch:
+    def __init__(self, state: LightState) -> None:
+        self.state = state
+
+    def switch(self) -> None:
+        self.state.switch()
+
+
+if __name__ == "__main__":
+    light = LightSwitch(OffState())
+    light.switch()
+    light.switch()
