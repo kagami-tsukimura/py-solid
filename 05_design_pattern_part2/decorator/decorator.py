d84f01055c8619b055492c2213d7b5e8ca079770
Kagami <shimura.kyohei7617@gmail.com>
2024-05-29 20:21:23 +0900

    auto: auto command push

--- 05_design_pattern_part2/decorator/decorator.py
+++ 05_design_pattern_part2/decorator/decorator.py
@@ -7,3 +7,10 @@ class Component(metaclass=ABCMeta):
     @abstractmethod
     def get_log_message(self, msg: str) -> str:
         pass
+
+
+class Logger(Component):
+
+    def get_log_message(self, msg: str) -> str:
+        now = datetime.datetime.now()
+        return f"[{now}]: {msg}"
