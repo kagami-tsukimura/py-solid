644e4c35396739e063d813822fecf283c4844a26
Kagami <shimura.kyohei7617@gmail.com>
2024-05-29 20:53:40 +0900

    auto: auto command push

--- 05_design_pattern_part2/proxy/proxy.py
+++ 05_design_pattern_part2/proxy/proxy.py
@@ -4,3 +4,8 @@ from abc import ABCMeta, abstractmethod
 class Server(metaclass=ABCMeta):
     def handle(self, user_id: str):
         pass
+
+
+class RealServer(Server):
+    def handle(self, user_id: str):
+        print(f"RealServer: handle request for {user_id}")
