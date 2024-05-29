ea250d4eae02660f7f8a974a7df11f24a6f03ad8
Kagami <shimura.kyohei7617@gmail.com>
2024-05-29 21:11:13 +0900

    auto: auto command push

--- 05_design_pattern_part2/bridge/bridge.py
+++ 05_design_pattern_part2/bridge/bridge.py
@@ -5,3 +5,8 @@ class MessageApp(metaclass=ABCMeta):
     @abstractmethod
     def send(self, message: str) -> None:
         pass
+
+
+class LINE(MessageApp):
+    def send(self, message: str) -> None:
+        print(f"[LINE] {message}")
