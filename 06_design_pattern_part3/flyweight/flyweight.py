bd8bea2dfea784c44c45332bd5de40387465ffe8
Kagami <shimura.kyohei7617@gmail.com>
2024-05-29 21:44:47 +0900

    auto: auto command push

--- 06_design_pattern_part3/flyweight/flyweight.py
+++ 06_design_pattern_part3/flyweight/flyweight.py
@@ -1,3 +1,11 @@
 class Stamp:
     def __init__(self, char: str) -> None:
         self.__char = char
+
+    def print_char(self) -> None:
+        print(self.__char)
+
+
+class StampFactory:
+    def __init__(self) -> None:
+        self.__pool = {}
