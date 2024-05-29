a7abeb8f2f1fbf38e5d6a9f48a975ec79767b0a9
Kagami <shimura.kyohei7617@gmail.com>
2024-05-29 20:55:46 +0900

    auto: auto command push

--- 05_design_pattern_part2/proxy/proxy.py
+++ 05_design_pattern_part2/proxy/proxy.py
@@ -9,3 +9,14 @@ class Server(metaclass=ABCMeta):
 class RealServer(Server):
     def handle(self, user_id: str):
         print(f"RealServer: handle request for {user_id}")
+
+
+class Proxy(Server):
+    def __init__(self, server: Server):
+        self.__server = server
+
+    def handle(self, user_id: str):
+        authorized_user_id = ["1", "2", "3"]
+        if not user_id in authorized_user_id:
+            raise Exception("Unauthorized user")
+        self.__server.handle(user_id)
