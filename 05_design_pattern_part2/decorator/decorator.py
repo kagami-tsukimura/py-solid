a880a795aa040d05b6f4d6fa52956ffd0f5c138d
Kagami <shimura.kyohei7617@gmail.com>
2024-05-29 20:21:09 +0900

    auto: auto command push

--- 05_design_pattern_part2/decorator/decorator.py
+++ 05_design_pattern_part2/decorator/decorator.py
@@ -1,2 +1,9 @@
 import datetime
 from abc import ABCMeta, abstractmethod
+
+
+class Component(metaclass=ABCMeta):
+
+    @abstractmethod
+    def get_log_message(self, msg: str) -> str:
+        pass
