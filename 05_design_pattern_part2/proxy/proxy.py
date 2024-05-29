7cdba3480b531a280cada334c539e328ebe15f21
Kagami <shimura.kyohei7617@gmail.com>
2024-05-29 20:56:51 +0900

    auto: auto command push

--- 05_design_pattern_part2/proxy/proxy.py
+++ 05_design_pattern_part2/proxy/proxy.py
@@ -2,25 +2,25 @@ from abc import ABCMeta, abstractmethod
 
 
 class Server(metaclass=ABCMeta):
-    def handle(self, user_id: str):
+    def handle(self, user_id: str) -> None:
         pass
 
 
 class RealServer(Server):
-    def handle(self, user_id: str):
+    def handle(self, user_id: str) -> None:
         print(f"RealServer: handle request for {user_id}")
 
 
 class Proxy(Server):
-    def __init__(self, server: Server):
+    def __init__(self, server: Server) -> None:
         self.__server = server
 
-    def _authorize(self, user_id: str):
+    def _authorize(self, user_id: str) -> None:
         authorized_user_id = ["1", "2", "3"]
         if not user_id in authorized_user_id:
             raise Exception("Unauthorized user")
 
-    def handle(self, user_id: str):
+    def handle(self, user_id: str) -> None:
         self._authorize(user_id)
         self.__server.handle(user_id)
 
