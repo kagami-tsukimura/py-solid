93236f97a3d84a2c8bc4c210632cac9617f16010
Kagami <shimura.kyohei7617@gmail.com>
2024-05-30 19:02:34 +0900

    auto: auto command push

--- 06_design_pattern_part3/interpreter/interpreter.py
+++ 06_design_pattern_part3/interpreter/interpreter.py
@@ -3,6 +3,12 @@ import re
 from abc import ABCMeta, abstractmethod
 
 
+class Context:
+    def __init__(self, expression: str, date: datetime.date):
+        self.__expression = expression
+        self.__date = date
+
+
 class AbstractExpression(metaclass=ABCMeta):
     def interpret(self, context: Context) -> None:
         pass
