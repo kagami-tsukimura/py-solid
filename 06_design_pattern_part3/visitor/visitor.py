b15d31a71e31b7fbf4a37908f51a10aeebe8c6f6
Kagami <shimura.kyohei7617@gmail.com>
2024-05-30 17:50:38 +0900

    auto: auto command push

--- 06_design_pattern_part3/visitor/visitor.py
+++ 06_design_pattern_part3/visitor/visitor.py
@@ -1,3 +1,9 @@
 from __future__ import annotations
 
 from abc import ABCMeta, abstractmethod
+
+
+class Entry(metaclass=ABCMeta):
+    def __init__(self, code: str, name: str) -> None:
+        self.__code = code
+        self.__name = name
