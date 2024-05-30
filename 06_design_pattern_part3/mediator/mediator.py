344a4feb0255a4d885ad7bff9f35a8530188274f
Kagami <shimura.kyohei7617@gmail.com>
2024-05-30 15:20:42 +0900

    auto: auto command push

--- 06_design_pattern_part3/mediator/mediator.py
+++ 06_design_pattern_part3/mediator/mediator.py
@@ -1,3 +1,9 @@
 from __future__ import annotations
 
 from abc import ABCMeta, abstractmethod
+
+
+class Mediator(metaclass=ABCMeta):
+    @abstractmethod
+    def register_user(self, user: User) -> None:
+        pass
