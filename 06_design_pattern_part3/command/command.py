8bc3cc3eb248645e3e799889e63b699916e94fd5
Kagami <shimura.kyohei7617@gmail.com>
2024-05-30 18:48:53 +0900

    auto: auto command push

--- 06_design_pattern_part3/command/command.py
+++ 06_design_pattern_part3/command/command.py
@@ -1,6 +1,14 @@
 from abc import ABCMeta, abstractmethod
 
 
+class File:
+    def __init__(self, name: str) -> None:
+        self.__name = name
+
+    def open(self) -> None:
+        print(f"open file: {self.__name}")
+
+
 class Command(metaclass=ABCMeta):
     @abstractmethod
     def execute(self) -> None:
